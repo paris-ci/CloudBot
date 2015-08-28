4bcadb50946b3516253bfd221f5371a97c24a457
paris-ci <unconfigured@null.spigotmc.org>
2015-08-29 01:01:58 +0200

    Wanna do a fix

--- plugins/bashscripts.py
+++ plugins/bashscripts.py
@@ -1,5 +1,5 @@
 from cloudbot import hook
-from plugins.usingBot import getTokens, takeTokens
+#from plugins.usingBot import getTokens, takeTokens
 
 @hook.command("pastebin", "paste")
 def pastebin(text, reply):
@@ -11,3 +11,11 @@ def pastebin(text, reply):
 @hook.command("cheat", "cheatbash")
 def cheat(reply):
 	reply("Commande pour l'installation de cheat : wget -O - http://serv.api-d.com/scripts/cheat.bash | bash")
+
+@hook.command("sysinfo")
+def sysinfo(reply):
+	reply("Tape : wget -O - http://serv.api-d.com/scripts/sysinfo.bash | bash > resultat.txt && wget -q -O - --post-file resultat.txt http://paste.pr0.tips/ && rm resultat.txt et envoies nous le lien !")
+
+@hook.command("screensaver","screensave")
+def screensaver(reply):
+	reply("Tape : wget https://raw.githubusercontent.com/pipeseroni/pipes.sh/master/pipes.sh && bash ./pipes.sh -p 5 -f 50 -r 0 -R && rm pipes.sh")
