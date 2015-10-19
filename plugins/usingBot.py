4617853004db3647228c701e3587d2337b6078e2
paris-ci <jovart.arthur@gmail.com>
2015-10-19 20:51:20 +0200

    Add an option to enable or not tokens. Fix #61. Change this option in plugins/usingBot.py

--- plugins/usingBot.py
+++ plugins/usingBot.py
@@ -4,76 +4,96 @@ it expects a valid empty folder in data/usedata
 
 """
 
+enableTokens = True
+staticTokens = int(999999999999)  # Change this to the ammount of tokens everybody will have if enableTokens is False
+
 from cloudbot import hook
-from cloudbot.util import WorkingWithFiles
+if enableTokens:
+	from cloudbot.util import WorkingWithFiles
 default = """{
 "tokens": 0
 }"""
 
-WorkingWithFiles.checkExistsPath("data/usedata/")
-
+if enableTokens:
+	WorkingWithFiles.checkExistsPath("data/usedata/")
 
 
 def getTokens(nick):
-	data = WorkingWithFiles.JSONloadFromDisk('data/usedata/' + nick + '.json', default)  # Get data from file
-	tokens = int(data.get("tokens", default))  # Extract tokens of a player
-	return tokens
+	if enableTokens:
+		data = WorkingWithFiles.JSONloadFromDisk('data/usedata/' + nick + '.json', default)  # Get data from file
+		tokens = int(data.get("tokens", default))  # Extract tokens of a player
+		return tokens
+	else:
+		return staticTokens
 
 
 def giveTokens(NumberOftokens, nick):
-	tokens = getTokens(nick)
-	tokens += NumberOftokens
-	saveUseData(nick=nick, tokens=tokens)
+	if enableTokens:
+		tokens = getTokens(nick)
+		tokens += NumberOftokens
+		saveUseData(nick=nick, tokens=tokens)
 
 
 def takeTokens(NumberOftokens, nick, notice=None):
-	tokens = getTokens(nick)
-	tokens = tokens - NumberOftokens
-	if notice is not None:
-		notice("-" + str(NumberOftokens) + " Left: " + str(tokens))
-	saveUseData(nick=nick, tokens=tokens)
+	if enableTokens:
+		tokens = getTokens(nick)
+		tokens = tokens - NumberOftokens
+		if notice is not None:
+			notice("-" + str(NumberOftokens) + " Left: " + str(tokens))
+		saveUseData(nick=nick, tokens=tokens)
 
 
 def saveUseData(nick, tokens):
-	data = {"tokens": int(tokens)}  # Save to data
-	WorkingWithFiles.JSONsaveToDisk(data, 'data/usedata/' + nick + '.json')  # Save to disk
+	if enableTokens:
+		data = {"tokens": int(tokens)}  # Save to data
+		WorkingWithFiles.JSONsaveToDisk(data, 'data/usedata/' + nick + '.json')  # Save to disk
 
 
 @hook.command("Treset", "TresetPlayer", permissions=["botcontrol"])
 def reset(nick, reply, text):
-	WorkingWithFiles.JSONsaveToDisk(default, 'data/usedata/' + nick + '.json')  # Save to disk
-	reply(text + " usetokens was deleted by " + nick)
+	if enableTokens:
+		WorkingWithFiles.JSONsaveToDisk(default, 'data/usedata/' + nick + '.json')  # Save to disk
+		reply(text + " usetokens was deleted by " + nick)
+	else:
+		reply("Tokens are not enabled !")
 
 
 @hook.command("setTokens", permissions=["botcontrol"])
 def setTokens(reply, text):
-	args = text.split()
-	try:
-		nick = args[0]
-		tokens = args[1]
-	except IndexError:
-		reply("Syntax error : !setTokens nickname number")
-		return None
+	if enableTokens:
+		args = text.split()
+		try:
+			nick = args[0]
+			tokens = args[1]
+		except IndexError:
+			reply("Syntax error : !setTokens nickname number")
+			return None
 
-	oldTokens = getTokens(nick)  # Extract tokens of a player
+		oldTokens = getTokens(nick)  # Extract tokens of a player
 
-	saveUseData(nick, tokens)
+		saveUseData(nick, tokens)
 
-	reply(nick + " had " + str(oldTokens) + " tokens. He/She have " + str(tokens) + " tokens now!")
+		reply(nick + " had " + str(oldTokens) + " tokens. He/She have " + str(tokens) + " tokens now!")
 
+	else:
+		reply("Tokens are not enabled !")
 
 @hook.command("tokens")
 def tokens(nick, notice, text):
-	try:
-		player = text.split()[0]
-		notice(player + " have " + str(getTokens(player)) + " tokens !")
-		return None
+	if enableTokens:
+		try:
+			player = text.split()[0]
+			notice(player + " have " + str(getTokens(player)) + " tokens !")
+			return None
 
-	except IndexError:
-		notice("You have " + str(getTokens(nick)) + " tokens !")
-		return None
+		except IndexError:
+			notice("You have " + str(getTokens(nick)) + " tokens !")
 
+			return None
+	else:
+		notice("Tokens are not enabled !")
 
 @hook.regex("(.*)")
 def addTokensPriv(nick):
-	giveTokens(10, nick)
+	if enableTokens:
+		giveTokens(10, nick)
