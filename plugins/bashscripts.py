f13dfc80105788b92a495055f2cbac978e9b742c
paris-ci <jovart.arthur@gmail.com>
2015-10-29 00:22:14 +0100

    delete some bashscripts; they are now included in the faq. You should do the same

--- plugins/bashscripts.py
+++ plugins/bashscripts.py
@@ -17,29 +17,6 @@ def cpastebin(text, reply):
     reply(
         "Tape : " + text + " > stdout.txt && wget -q -O - --post-file stdout.txt http://paste.pr0.tips/ && rm stdout.txt et envoies nous le lien !")
 
-
-@hook.command("cheat", "cheatbash")
-def cheat(reply):
-    reply("Commande pour l'installation de cheat : wget -O - http://serv.api-d.com/scripts/cheat.bash | bash")
-
-
-@hook.command("sysinfo")
-def sysinfo(reply):
-    reply(
-        "Tape : wget -O - http://serv.api-d.com/scripts/sysinfo.bash | bash > resultat.txt && wget -q -O - --post-file resultat.txt http://paste.pr0.tips/ && rm resultat.txt et envoies nous le lien !")
-
-
-@hook.command("screensaver", "screensave")
-def screensaver(reply):
-    reply(
-        "Tape : wget https://raw.githubusercontent.com/pipeseroni/pipes.sh/master/pipes.sh && bash ./pipes.sh -p 5 -f 50 -r 0 -R && rm pipes.sh")
-
-
-@hook.command("installjava8", "java8")
-def java8(reply):
-    reply("Commande pour l'installation de java 8 : wget -O - http://serv.api-d.com/scripts/java.bash | bash")
-
-
 @hook.command("listdirs", "listdir", "tree", "treelist", "dirtree", "dirlist")
 def dirlist(reply):
     reply("Commande pour voir le contenu d'un repertoire : tape")
